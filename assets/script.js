bd9f4b68560dd591d9614270e49baaa51cb26205
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-01 15:54:44 -0800

    more basic implementation

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,12 @@
 let timer =;
-
+let input = document.getElementById()
 let timedisplay = lorem;
+var answer = n
+var score = x
+
+function addscore() {
+    if (answer == true) {
+        x++;
+    }
+}
+
