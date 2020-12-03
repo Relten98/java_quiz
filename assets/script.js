8b9897cad0957715b72d403d27a25333a698c1a1
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-03 15:33:58 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,17 @@
 var questions = ["What's 9 x 10?", "How much wood can a woodchuck chuck?"]
+var i = 0;
+
 
 let x = document.getElementById("score").innerHTML;
 
 
 
 function beginQuiz() {
+
     document.getElementById("btn").innerHTML = "Submit Answer";
     document.getElementById("qHeader").innerHTML = questions;
     document.getElementById("qHeader2").innerHTML = "";
-    return
+    return;
 };
 
 
