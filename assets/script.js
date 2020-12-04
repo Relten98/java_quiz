16ac34a8cbfe84d97a26b725a172d33b6133b7ff
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-04 12:41:59 -0800

    IMPROVED COMMENTS
    Scoring system is now functional, same with timer penalties.

--- assets/script.js
+++ assets/script.js
@@ -4,20 +4,45 @@
 var i = 0;
 let x = 0
 
+/////////////////////////////////////////
+/// QUESTION VARS
+////////////////////////////////////////
+
+
 /// These are just letter values don't mind them.
 let a = "";
 let b = "";
 let c = "";
 let d = "";
+
+/// Answer vars
+
+/// n stands for user input, while correct is defined by question
+let n = "";
+
+let correctanswer = "";
+
+//////////////////////////////////////////////////
+///////////////// SCORE CHECKER /////////////////
+////////////////////////////////////////////////
+
 document.getElementById("score").innerHTML = "Score" + " : " + x;
 
 var start = document.getElementById("begin").addEventListener("click", setTime);
 
 function addscore() {
-    document.getElementById("score").innerHTML = "Score" + " : " + x;
+    document.getElementById("score").innerHTML = "Score" + " : " + x++;
     return;
 }
 
+function validator() {
+    /// Checks to see if it is correct.
+    if (correctanswer = n){
+        addscore();
+    } else {
+        timeLeft - 50;
+    }
+}
 
 //// Actually, screw that.... I be commentin' out this shiznit fer now.
 
@@ -32,7 +57,8 @@ document.getElementById("timer").innerHTML = "Time Left" + " : " + timeLeft;
 function setTime() {
     /// This is here to swap the button to the submittor btn.
     document.getElementById('begin').id = "begin2";
-    document.getElementById("begin2").addEventListener("click", setTime);
+    document.getElementById("begin2").innerHTML = "Submit"
+    document.getElementById("begin2").addEventListener("click", addscore);
 
     /// Timer stuff.
     var timerInterval = setInterval(function () {
@@ -61,6 +87,7 @@ function endQuiz() {
     document.getElementById("btn-d").innerHTML = "";
 
     /// Sets up the end, and allows the form to be resetted.
+    timeLeft + 2600;
     document.getElementById('resetter').innerHTML = "Try Again?";
     document.getElementById("resetter").addEventListener("click", resetQuiz);
     document.getElementById("scorepage").innerHTML = "Your score is : " + x;
@@ -69,13 +96,13 @@ function endQuiz() {
 
 //// Default values
 function resetQuiz() {
-
+    timeLeft = 2600;
+    x = 0;
     document.getElementById("begin2").innerHTML = "Begin";
     document.getElementById('begin2').id = "begin";
     document.getElementById("qHeader").innerHTML = "The Java Quiz";
     document.getElementById('resetter').innerHTML = "";
     document.getElementById("scorepage").innerHTML = ""
-    timeLeft = 2600
     return;
 };
 
