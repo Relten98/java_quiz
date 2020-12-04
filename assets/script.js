976968de1c5fe4ae5740b4561da7c893f2db8ba9
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-03 21:02:20 -0800

    linkage
    Linked CSS file, more js changes

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,14 @@
 var questions = ["What's 9 x 10?", "How much wood can a woodchuck chuck?"]
 var i = 0;
-
-
+let document.getElementById("qHeader").innerHTML = "The Java Quiz";
+let document.getElementById("qHeader2").innerHTML = "";
 let x = document.getElementById("score").innerHTML;
 
 
 
 function beginQuiz() {
-
-    document.getElementById("btn").innerHTML = "Submit Answer";
     document.getElementById("qHeader").innerHTML = questions;
+    document.getElementById("btn").innerHTML = "Submit Answer";
     document.getElementById("qHeader2").innerHTML = "";
     return;
 };
